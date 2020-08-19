17233a3ed17e556305a2a53393e2a5b4f1ec772d
todirbg <todirbg@yahoo.com>
2020-08-19 11:56:12 +0300

    Added SASL aditional plugin

---
+++ Plugin/Radar/data/modules/Custom Module/Radar_2D.lua
@@ -0,0 +1,474 @@
+size = {1002, 1060}
+
+
+local brt_dr = globalPropertyf("custom/rdr4b/brightness")
+local tilt_dr = globalPropertyf("custom/rdr4b/tilt")
+local gain_dr = globalPropertyf("custom/rdr4b/gain")
+local mode_dr = globalPropertyi("custom/rdr4b/mode")
+local range_dr = globalPropertyi("custom/rdr4b/range")
+local stab_dr = globalPropertyi("custom/rdr4b/stab")
+
+
+local radar = sasl.gl.loadImage("radar.png", 0, 0, 1002, 1060)
+local brt_knob = sasl.gl.loadImage("radar.png", 1067, 295, 65, 65)
+local gain_knob = sasl.gl.loadImage("radar.png", 1067, 405, 65, 65)
+local tilt_knob = sasl.gl.loadImage("radar.png", 1029, 510, 141, 141)
+local mode_knob = sasl.gl.loadImage("radar.png", 1029, 685, 141, 141)
+local range_knob = sasl.gl.loadImage("radar.png", 1029, 854, 141, 141)
+local stab_butt = sasl.gl.loadImage("radar.png", 1080, 212, 39, 32)
+
+local mouse_timer = sasl.createTimer ()
+
+components = {
+    textureLit {
+      position = {148, 223, 706, 507},
+      image =     get(dis),
+      brt2 = 1, --function() return get(brt_dr) end,
+      x = 706,
+      y = 507,
+    },  
+	
+    texture {
+      position = {0, 0, 1002, 1060},
+      image =     get(radar),
+      brt2 = 1, --function() return get(brt_dr) end,
+    },  	
+	
+    textureRotate {
+  
+      position = {481, 854, 141, 141},
+      image = get(mode_knob),
+      angle = function()
+                  return get(mode_dr)*35
+                end,
+      brt2 = 1,
+    },
+	
+    textureRotate {
+  
+      position = {170, 854, 141, 141},
+      image = get(range_knob),
+      angle = function()
+                  return get(range_dr)*40
+                end,
+      brt2 = 1,
+    },
+	
+    textureRotate {
+  
+      position = {821, 854, 141, 141},
+      image = get(tilt_knob),
+      angle = function()
+                  return get(tilt_dr)*10
+                end,
+      brt2 = 1,
+    },
+    
+    textureRotate {
+  
+      position = {50, 854, 65, 65},
+      image = get(brt_knob),
+      angle = function()
+                  return get(brt_dr)*270
+                end,
+      brt2 = 1,
+    },
+    
+    textureRotate {
+  
+      position = {373, 867, 65, 65},
+      image = get(gain_knob),
+      angle = function()
+                  return get(gain_dr)*270
+                end,
+     brt2 = 1,
+    },  
+    
+    texture {
+  
+      position = function() 
+        if get(stab_dr) == 1 then return {686, 839, 40, 33}
+         else return {654, 839, 40, 33}
+         end
+      end,
+      image = get(stab_butt),
+      brt2 = 1,
+    },
+    
+    clickable {
+		position = {148, 223, 706, 507},
+    
+		cursor = {
+		  x =  -15.5,
+		  y =  -16.5,
+		  width = 31,
+		  height = 33,
+		  shape = sasl.gl.loadImage("mc_handle.png"),
+		  hideOSCursor = true
+		  },
+    
+		onMouseDown = function(component , x, y, button , parentX , parentY)
+		  Radar_2D_context:setIsVisible(false)
+		  return false
+		end , 
+    },    
+	
+    clickable {
+    position = {647, 830, 87, 50},
+		cursor = {
+		  x =  -15.5,
+		  y =  -16.5,
+		  width = 31,
+		  height = 33,
+		  shape = sasl.gl.loadImage("mc_handle.png"),
+		  hideOSCursor = true
+		  },   
+    onMouseDown = function(component , x, y, button , parentX , parentY)
+      if get(stab_dr) == 1 then set(stab_dr, 0)
+	  elseif get(stab_dr) == 0 then set(stab_dr, 1)
+	  end
+      
+      return false
+    end  
+    },
+	
+	
+    clickable {
+    position = {482, 857, 70, 136 },
+    
+    cursor = {
+      x =  -16,
+      y =  -23.5,
+      width = 32,
+      height = 47,
+      shape = sasl.gl.loadImage("mc_rot3_lft.png"),
+      hideOSCursor = true
+      },
+    
+    onMouseWheel = function(x,y,w,h,r,t,clicks)
+      if clicks > 0 then set_dr_limit(mode_dr,1, 0, 4)
+      elseif clicks < 0 then set_dr_limit(mode_dr,-1, 0, 4)
+	  end
+      
+      return false
+    end,
+	
+    onMouseDown = function(component , x, y, button , parentX , parentY)
+		set_dr_limit(mode_dr,-1, 0, 4)
+      return false
+    end
+    },
+	
+    clickable {
+    position = {552, 857, 70, 136  },
+    
+    cursor = {
+      x =  -16,
+      y =  -23.5,
+      width = 32,
+      height = 47,
+      shape = sasl.gl.loadImage("mc_rot3_rgt.png"),
+      hideOSCursor = true
+      },
+    
+    onMouseWheel = function(x,y,w,h,r,t,clicks)
+      if clicks > 0 then set_dr_limit(mode_dr,1, 0, 4)
+      elseif clicks < 0 then set_dr_limit(mode_dr,-1, 0, 4)
+	  end      
+      return false
+    end,
+    onMouseDown = function(component , x, y, button , parentX , parentY)
+      set_dr_limit(mode_dr,1, 0, 4)
+      
+      return false
+    end    
+    },
+	
+    clickable {
+    position = {170, 857, 70, 136 },
+    
+    cursor = {
+      x =  -16,
+      y =  -23.5,
+      width = 32,
+      height = 47,
+      shape = sasl.gl.loadImage("mc_rot3_lft.png"),
+      hideOSCursor = true
+      },
+    
+    onMouseWheel = function(x,y,w,h,r,t,clicks)
+      if clicks > 0 then set_dr_limit(range_dr,1, 0, 5)
+      elseif clicks < 0 then set_dr_limit(range_dr,-1, 0, 5)
+	  end      
+      return false
+    end,
+    onMouseDown = function(component , x, y, button , parentX , parentY)
+		set_dr_limit(range_dr,-1, 0, 5)
+      
+      return false
+    end    
+    },
+	
+    clickable {
+    position = {240, 857, 70, 136  },
+    
+    cursor = {
+      x =  -16,
+      y =  -23.5,
+      width = 32,
+      height = 47,
+      shape = sasl.gl.loadImage("mc_rot3_rgt.png"),
+      hideOSCursor = true
+      },
+    
+    onMouseWheel = function(x,y,w,h,r,t,clicks)
+      if clicks > 0 then set_dr_limit(range_dr,1, 0, 5)
+      elseif clicks < 0 then set_dr_limit(range_dr,-1, 0, 5)
+ 	  end     
+      return false
+    end,
+    onMouseDown = function(component , x, y, button , parentX , parentY)
+      set_dr_limit(range_dr,1, 0, 5)
+      
+      return false
+    end    
+    },
+	
+   clickable {
+    position = {822, 857, 70, 136},
+    
+    cursor = {
+      x =  -16,
+      y =  -23.5,
+      width = 32,
+      height = 47,
+      shape = sasl.gl.loadImage("mc_rot3_lft.png"),
+      hideOSCursor = true
+      },
+    
+    onMouseWheel = function(x,y,w,h,r,t,clicks)
+      if clicks > 0 then set_dr_limit(tilt_dr, 0.1, -15, 15)
+      elseif clicks < 0 then set_dr_limit(tilt_dr, -0.1, -15, 15)
+      end
+      
+      return false
+    end,
+    onMouseHold = function(component , x, y, button , parentX , parentY)
+      set_dr_limit_timeout(tilt_dr, -0.1, -15, 15)
+      return false
+    end,  	
+    onMouseDown = function(component , x, y, button , parentX , parentY)
+      set_dr_limit(tilt_dr, -0.1, -15, 15)
+	  sasl.startTimer(mouse_timer)
+      return false
+    end,  
+  	onMouseUP = function(component , x, y, button , parentX , parentY)
+		sasl.stopTimer(mouse_timer)
+	end,	
+    },
+	
+    clickable {
+    position = {892, 857, 70, 136  },
+    
+    cursor = {
+      x =  -16,
+      y =  -23.5,
+      width = 32,
+      height = 47,
+      shape = sasl.gl.loadImage("mc_rot3_rgt.png"),
+      hideOSCursor = true
+      },
+    
+    onMouseWheel = function(x,y,w,h,r,t,clicks)
+      if clicks > 0 then set_dr_limit(tilt_dr, 0.1, -15, 15)
+      elseif clicks < 0 then set_dr_limit(tilt_dr, -0.1, -15, 15)
+ 	  end     
+      return false
+    end,
+    onMouseHold = function(component , x, y, button , parentX , parentY)
+      set_dr_limit_timeout(tilt_dr, 0.1, -15, 15)
+      return false
+    end,  
+    onMouseDown = function(component , x, y, button , parentX , parentY)
+      set_dr_limit(tilt_dr, 0.1, -15, 15)
+	  sasl.startTimer(mouse_timer)
+      return false
+    end,
+ 	onMouseUP = function(component , x, y, button , parentX , parentY)
+		sasl.resetTimer(mouse_timer)
+	end,	
+    },
+	
+   clickable {
+    position = {365, 862, 40, 74 },
+    
+    cursor = {
+      x =  -16,
+      y =  -23.5,
+      width = 32,
+      height = 47,
+      shape = sasl.gl.loadImage("mc_rot3_lft.png"),
+      hideOSCursor = true
+      },
+    
+    onMouseWheel = function(x,y,w,h,r,t,clicks)
+      if clicks > 0 then set_dr_limit(gain_dr, 0.05, 0, 1)
+      elseif clicks < 0 then set_dr_limit(gain_dr, -0.05, 0, 1)
+      end
+      
+      return false
+    end,
+    onMouseHold= function()
+      set_dr_limit_timeout(gain_dr, -0.05, 0, 1)
+      return false
+    end,
+    onMouseDown = function(component , x, y, button , parentX , parentY)
+      set_dr_limit(gain_dr, -0.05, 0, 1)
+	  sasl.startTimer(mouse_timer)
+      return false
+    end,	
+ 	onMouseUP = function(component , x, y, button , parentX , parentY)
+		sasl.resetTimer(mouse_timer)
+	end,
+    },
+	
+    clickable {
+    position = {405, 862, 40, 74},
+    
+    cursor = {
+      x =  -16,
+      y =  -23.5,
+      width = 32,
+      height = 47,
+      shape = sasl.gl.loadImage("mc_rot3_rgt.png"),
+      hideOSCursor = true
+      },
+    
+    onMouseWheel = function(x,y,w,h,r,t,clicks)
+      if clicks > 0 then set_dr_limit(gain_dr, 0.05, 0, 1)
+      elseif clicks < 0 then set_dr_limit(gain_dr, -0.05, 0, 1)
+      end
+      
+      return false
+    end,
+    onMouseHold = function(component , x, y, button , parentX , parentY)
+      set_dr_limit_timeout(gain_dr, 0.05, 0, 1)
+      return false
+    end,    	
+    onMouseDown = function(component , x, y, button , parentX , parentY)
+      set_dr_limit(gain_dr, 0.05, 0, 1)
+	  sasl.startTimer(mouse_timer)
+      return false
+    end,
+ 	onMouseUP = function(component , x, y, button , parentX , parentY)
+		sasl.resetTimer(mouse_timer)
+	end,
+    },
+	
+   clickable {
+    position = {42, 850, 40, 74},
+    
+    cursor = {
+      x =  -16,
+      y =  -23.5,
+      width = 32,
+      height = 47,
+      shape = sasl.gl.loadImage("mc_rot3_lft.png"),
+      hideOSCursor = true
+      },
+    
+    onMouseWheel = function(x,y,w,h,r,t,clicks)
+      if clicks > 0 then set_dr_limit(brt_dr,0.05, 0, 1)
+      elseif clicks < 0 then set_dr_limit(brt_dr, -0.05, 0, 1)
+      end
+      
+      return false
+    end,
+    onMouseHold = function(component , x, y, button , parentX , parentY)
+      set_dr_limit_timeout(brt_dr, -0.05, 0, 1)
+      return false
+    end,   	
+    onMouseDown = function(component , x, y, button , parentX , parentY)
+      set_dr_limit(brt_dr, -0.05, 0, 1)
+	  sasl.startTimer(mouse_timer)
+      return false
+    end,
+ 	onMouseUP = function(component , x, y, button , parentX , parentY)
+		sasl.resetTimer(mouse_timer)
+	end,
+    },
+	
+    clickable {
+    position = {83, 850, 40, 74},
+    
+    cursor = {
+      x =  -16,
+      y =  -23.5,
+      width = 32,
+      height = 47,
+      shape = sasl.gl.loadImage("mc_rot3_rgt.png"),
+      hideOSCursor = true
+      },
+    
+    onMouseWheel = function(x,y,w,h,r,t,clicks)
+      if clicks > 0 then set_dr_limit(brt_dr, 0.05, 0, 1)
+      elseif clicks < 0 then set_dr_limit(brt_dr, -0.05, 0, 1)
+      end
+      
+      return false
+    end,
+    onMouseHold = function(component , x, y, button , parentX , parentY)
+      set_dr_limit_timeout(brt_dr, 0.05, 0, 1)
+      return false
+    end,   	
+    onMouseDown = function(component , x, y, button , parentX , parentY)
+      set_dr_limit(brt_dr, 0.05, 0, 1)
+	  sasl.startTimer(mouse_timer)
+      return false
+    end,
+	onMouseUP = function(component , x, y, button , parentX , parentY)
+		sasl.resetTimer(mouse_timer)
+	end,
+    },
+
+}
+
+function set_dr_limit(dr, step, limit_low, limit_hi)
+	local value = get(dr)
+	value = value + step
+	if value >= limit_low and value <= limit_hi then
+		set(dr, value)
+	else
+		if step > 0 then
+			set(dr, limit_hi)
+		else
+			set(dr, limit_low)
+		end
+	end
+end
+
+function set_dr_limit_timeout(dr, step, limit_low, limit_hi)
+	if sasl.getElapsedMicroseconds(mouse_timer) >= 500 then
+		local value = get(dr)
+		value = value + step
+		if value >= limit_low and value <= limit_hi then
+			set(dr, value)
+		else
+			if step > 0 then
+				set(dr, limit_hi)
+			else
+				set(dr, limit_low)
+			end
+		end
+	end
+end
+
+
+function draw()
+
+    if Radar_2D_context:isVisible () then
+	sasl.gl.drawRectangle(148, 223, 706, 507, {0 ,0 ,0 ,1})
+    drawAll(components)
+    end
+
+end
---
+++ Plugin/Radar/data/modules/main.lua
@@ -0,0 +1,102 @@
+--main.lua
+size = { 2048, 2048 }
+
+dis = sasl.gl.createTexture(458 , 329)
+
+addSearchPath(sasl.getXPlanePath ().."/Resources/bitmaps/interface")
+addSearchPath(sasl.getXPlanePath ().."/Resources/bitmaps/interface11")
+
+local popout_img = sasl.gl.loadImage("floating_window@1.5x.png", 27, 0, 28, 28)
+local close_img = sasl.gl.loadImage("floating_window@1.5x.png", 0, 28, 26, 26)
+local hide_ui = true
+
+components = {
+    Radar {},
+    }
+
+	
+Radar_2D_context = contextWindow {
+    name      = "Radar",
+    position    = {50, 50, 1002, 1060},
+    savePosition = true,  
+    minimumSize = {1002/4, 1060/4},
+    maximumSize = {1002, 1060},
+    noDecore = false,
+	customDecore = true,
+	noBackground = true,
+    resizeMode = SASL_CW_RESIZE_RIGHT_BOTTOM, 
+	proportional = true,
+    visible     = false,
+	saveState = true,
+        components = {
+            Radar_2D {
+                position    = {0, 0, 1002, 1060}
+            }
+          },
+		  
+	decoration = {
+		headerHeight = 0 ,
+			
+			 main = {
+				draw = function(w, h)-- draw  window  header...
+					if hide_ui == false then
+					sasl.gl.drawTexture(popout_img, w-20, h-20, 20, 20)
+					sasl.gl.drawTexture(close_img, 0, h-20, 20, 20)
+					end
+				end, 
+				
+				onMouseMove = function(x, y, w, h)
+					if y > h - 20 and y < h and Radar_2D_context:isPoppedOut() == false then
+						hide_ui = false
+					else
+						hide_ui = true
+					end
+					
+				end ,
+				
+				onMouseDown = function(x, y, w, h, button)
+					if  Radar_2D_context:isPoppedOut() == false and button == MB_LEFT  then
+						if y > h - 20 and y < h then
+							if x > w - 20 then
+								Radar_2D_context:setMode(SASL_CW_MODE_POPOUT)
+							elseif x < 20 then
+								Radar_2D_context:setIsVisible(false)
+							end
+						end					
+					end
+
+				end ,				
+				
+			}
+	}	
+
+}
+
+local Radar2D_command = sasl.createCommand("custom/rdr4b/radar2D", "RDR-4B visible")
+function Radar2D_handler(phase)  -- for all commands phase equals: 0 on press; 1 while holding; 2 on release
+  if 0 == phase then
+    if Radar_2D_context:isVisible () then
+      Radar_2D_context:setIsVisible(false)
+    else
+      Radar_2D_context:setIsVisible(true)
+    end
+  end
+  return false
+end
+
+sasl.registerCommandHandler(Radar2D_command, 0, Radar2D_handler)
+
+menu_master = sasl.appendMenuItem (PLUGINS_MENU_ID, "RDR-4B Weather Radar" )
+menu_main = sasl.createMenu ("", PLUGINS_MENU_ID, menu_master)
+menu_option = sasl.appendMenuItem(menu_main, "Toggle RDR-4B 2D pop up", function() 
+                                                                        sasl.commandOnce(sasl.findCommand("custom/rdr4b/radar2D"))
+                                                                        return false
+                                                                        end)
+																		
+function draw()
+
+    if Radar_2D_context:isVisible () then
+	sasl.gl.getTargetTextureData(dis , 0, 0, 458 , 329)
+    end
+
+end
